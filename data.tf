35d38a7e8d819e0aa09ba3fb62d8e60d05de36bf
Yuriy S <yserinov@gmail.com>
2025-11-04 18:19:21 -0500

    initial commit

--- data.tf
+++ data.tf
@@ -1,8 +1,9 @@
 ########################################################
-# Enumerate all record sets in each Private DNS zone
+# Enumerate all record sets across multiple zones
+# (looping over each record type, then merging)
 ########################################################
 
-# Get zone resource IDs
+# 1) Resolve each zone (uses provider alias "hub")
 data "azurerm_private_dns_zone" "zones" {
   for_each = toset(var.zones)
 
@@ -11,17 +12,39 @@ data "azurerm_private_dns_zone" "zones" {
   provider            = azurerm.hub
 }
 
-# List all records for each zone
-data "azapi_resource_list" "all_records" {
-  for_each  = data.azurerm_private_dns_zone.zones
-  type      = "Microsoft.Network/privateDnsZones/ALL@2018-09-01"
-  parent_id = each.value.id
+# 2) Record types to enumerate (Private DNS supports these)
+locals {
+  record_types = ["A", "AAAA", "CNAME", "MX", "PTR", "SOA", "SRV", "TXT"]
 }
 
-# Collect outputs for readability
+# 3) Build (zone,type) combinations
+locals {
+  zone_type_pairs = flatten([
+    for zone_name, z in data.azurerm_private_dns_zone.zones : [
+      for rt in local.record_types : {
+        key       = "${zone_name}:${rt}"
+        zone_name = zone_name
+        zone_id   = z.id
+        type      = rt
+      }
+    ]
+  ])
+}
+
+# 4) List records per (zone,type) with AzAPI
+data "azapi_resource_list" "records" {
+  for_each  = { for p in local.zone_type_pairs : p.key => p }
+  type      = "Microsoft.Network/privateDnsZones/${each.value.type}@2018-09-01"
+  parent_id = each.value.zone_id
+}
+
+# 5) Merge per zone
 locals {
   zone_records = {
-    for zone_name, record_list in data.azapi_resource_list.all_records :
-    zone_name => try(record_list.output.value, [])
+    for zone_name, _ in data.azurerm_private_dns_zone.zones :
+    zone_name => flatten([
+      for rt in local.record_types :
+      try(data.azapi_resource_list.records["${zone_name}:${rt}"].output.value, [])
+    ])
   }
 }
--- main.tf
+++ main.tf
@@ -1,42 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 2.0"
-    }
-  }
-}
-
-# Provider alias for hub context
-provider "azurerm" {
-  alias    = "hub"
-  features {}
-}
-
-provider "azapi" {}
-
-########################################################
-# Variables
-########################################################
-
-variable "resource_group_name" {
-  description = "Static resource group name containing the private DNS zones"
-  type        = string
-}
-
-variable "zones" {
-  description = "List of Private DNS zone names to enumerate"
-  type        = list(string)
-}
-
-########################################################
-# Outputs
-########################################################
-
 output "record_names_by_zone" {
   description = "Simplified map of zone -> record names"
   value = {
@@ -46,7 +7,7 @@ output "record_names_by_zone" {
 }
 
 output "record_details_by_zone" {
-  description = "Detailed list of record JSON objects per zone"
+  description = "Detailed JSON of all record sets per zone"
   value       = local.zone_records
-  sensitive   = true # may contain internal IPs or metadata
+  sensitive   = true
 }

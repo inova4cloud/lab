f0b3f888c4a67e7d89b52f648711692a79e945e2
Yuriy S <yserinov@gmail.com>
2026-01-28 14:36:41 -0500

    New lab template

--- main.tf
+++ main.tf
@@ -1,33 +1,154 @@
-########################################################
-# Variables
-########################################################
 
-variable "resource_group_name" {
-  type        = string
-  description = "Resource Group containing the Private DNS zones"
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
 }
 
-variable "zones" {
-  type        = list(string)
-  description = "List of Private DNS zone names to enumerate"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.10.0.0/16"]
 }
 
-########################################################
-# Outputs
-########################################################
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
 
-# Simplified list of record names per zone
-output "record_names_by_zone" {
-  description = "List of record names in each Private DNS zone"
-  value = {
-    for zone_name, records in local.zone_records :
-    zone_name => [for r in records : r.name]
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  # SSH for admin access (lock down in real setups)
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = var.ssh_source_cidr
+    destination_address_prefix = "*"
+  }
+
+  # HTTP to VMs (traffic comes via LB, but rule still needed on NIC/subnet)
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
   }
 }
 
-# Full record payload (ARM JSON)
-output "record_details_by_zone" {
-  description = "Full ARM JSON of record sets per zone"
-  value       = local.zone_records
-  sensitive   = true
+resource "azurerm_subnet_network_security_group_association" "subnet_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_public_ip" "lb_pip" {
+  name                = "${var.prefix}-lb-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+
+  frontend_ip_configuration {
+    name                 = "fe"
+    public_ip_address_id = azurerm_public_ip.lb_pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bepool" {
+  name            = "be"
+  loadbalancer_id = azurerm_lb.lb.id
+}
+
+resource "azurerm_lb_probe" "http" {
+  name            = "http-probe"
+  loadbalancer_id = azurerm_lb.lb.id
+  protocol        = "Tcp"
+  port            = 80
+}
+
+resource "azurerm_lb_rule" "http" {
+  name                           = "http-rule"
+  loadbalancer_id                = azurerm_lb.lb.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "fe"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bepool.id]
+  probe_id                       = azurerm_lb_probe.http.id
+}
+
+resource "azurerm_network_interface" "nic" {
+  count               = var.vm_count
+  name                = "${var.prefix}-nic-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipcfg"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "nic_to_be" {
+  count                   = var.vm_count
+  network_interface_id    = azurerm_network_interface.nic[count.index].id
+  ip_configuration_name   = "ipcfg"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.bepool.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = var.vm_count
+  name                = "${var.prefix}-vm-${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  size                = var.vm_size
+
+  admin_username = var.admin_username
+
+  # Use SSH key (recommended)
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_public_key
+  }
+
+  network_interface_ids = [azurerm_network_interface.nic[count.index].id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  custom_data = base64encode(templatefile("${path.module}/cloud-init.yaml", {
+    hostname = "${var.prefix}-vm-${count.index}"
+  }))
 }
